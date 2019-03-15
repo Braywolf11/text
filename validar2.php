78e86f81adc5045f2d429bec7d7f724f6eec47ea
braywolf11 <brayfer1893@gmail.com>
2019-03-14 19:53:51 -0500

    actualizacion

---
+++ validar2.php
@@ -0,0 +1,46 @@
+<?php
+$nombre = $_POST['Nombres'];
+$apellido = $_POST['Apellidos'];
+$tipos = $_POST['Tipos'];
+$Nro = $_POST['Nro'];
+$masculino = $_POST['Masculino'];
+$femenino = $_POST['Femenino'];
+$telefono = $_POST['Telefono'];
+$celular = $_POST['Celular'];
+$departamentos= $_POST['Departamentos'];
+$municipios= $_POST['Municipios'];
+$correo = $_POST['Correo'];
+$contraseña= $_POST['Contraseña'];
+$confirmar = $_POST['Confirmar'];
+$foto= $_POST['Foto'];
+
+echo "El usuario es: ".$nombre;
+echo"<br>";
+echo "El usuario es: ".$apellido;
+echo"<br>";
+echo "El usuario es: ".$tipos;
+echo"<br>";
+echo "La contraseña es: ".$Nro;
+echo"<br>";
+echo "El usuario es: ".$masculino;
+echo"<br>";
+echo "La contraseña es: ".$femenino;
+echo"<br>";
+echo "El usuario es: ".$telefono;
+echo"<br>";
+echo "La contraseña es: ".$celular;
+echo"<br>";
+echo "El usuario es: ".$departamentos;
+echo"<br>";
+echo "La contraseña es: ".$municipios;
+echo"<br>";
+echo "El usuario es: ".$correo;
+echo"<br>";
+echo "La contraseña es: ".$contraseña;
+echo"<br>";
+echo "El usuario es: ".$confirmar;
+echo"<br>";
+echo "La contraseña es: ".$foto;
+echo"<br>";
+
+?>
