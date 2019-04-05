eca59e232d554060043b29043f057e6227753e55
braywolf11 <brayfer1893@gmail.com>
2019-04-04 20:38:04 -0500

    acualizacion

--- Scrips_Ejemplos/Bucles/bucle11.php
+++ Scrips_Ejemplos/Bucles/bucle11.php
@@ -1,4 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Bucles</title>
+    <link rel="stylesheet" type="text/css" media="screen" href="main.css">
+    <script src="main.js"></script>
+</head>
+<body bgcolor="#5fb1f5">
+    
+</body>
+</html>
+<center>
 <?php
+echo "<h2> 1) ";
+echo "<br>";
     echo " 1 ";
     echo "<br>";
     echo " 2 ";
@@ -20,6 +34,8 @@
     echo " 10";
     echo "<br>";
     echo "<hr>";
+echo "<h2> 2) ";
+echo "<br>";
     echo "<b>Imprecion de los numeros de el 1 - 10 </b>";
     echo "<table border = '2'>";
 
@@ -32,8 +48,9 @@
     echo "</table>";
     echo "<hr>";
     echo "<br><br>";
-
-    echo "<b>Inprecion de los numeros de el 1 - 10 </b><br><br>";
+echo "<h2> 3) ";
+echo "<br>";
+    echo "<b>Inprecion de los numeros de el 1 - 100 </b><br><br>";
     
         $j=1;
         while ($j <= 100) {
@@ -45,7 +62,9 @@
     
     echo "<hr>";
     echo "<br><br>";
-    echo "<b>Imprecion de los numeros de el 1 - 10 con for </b>";
+echo "<h2> 4) ";
+echo "<br>";
+    echo "<b>Imprecion de los numeros de el 1 - 100 en pares con for </b>";
     echo "<table border = '2'>";
 
     $k=2;
@@ -64,7 +83,9 @@
     echo "</table>";
     echo "<hr>";
     echo "<br><br>";
-    echo "<b>Inprecion de los numeros de el 1 - 10 </b><br><br>";
+echo "<h2> 5) ";
+echo "<br>";
+    echo "<b>Inprecion de numeros aleatorios</b><br><br>";
     echo "<table border = '2'>";
         $j=1;
         while ($j <= 10) {
@@ -74,7 +95,8 @@
     echo "</table>";
     echo "<hr>";
     echo "<br><br>";
-    
+echo "<h2> 6) ";
+echo "<br>";    
         $acum=0;
          for ($i=1;$i<=100;$i++){
                 echo " <td> ";
@@ -85,8 +107,10 @@
     }
     echo "<br><br> La suma es : $acum";
     echo "<hr>";
-    echo "<br><br>"; 
-    echo "<b>Inprecion de los numeros de el 1 - 10 </b><br><br>";
+    echo "<br><br>";
+echo "<h2> 7) ";
+echo "<br>"; 
+    echo "<b>Imprecion de los multiplos de 3 </b><br><br>";
     echo "<table border = '2'>";
     for($i=1;$i<=100;$i++)
     {
@@ -108,7 +132,9 @@
     echo "</table>";
     echo "<hr>";
     echo "<br><br>";
-    echo "<b>Inprecion de los numeros de el 1 - 10 </b><br><br>";
+echo "<h2> 8) ";
+echo "<br>";
+    echo "<b>Inprecion del total de la suma de los numeros aleatorios impares y pares </b><br><br>";
     echo "<table border = '2'>";
         $j=1;
         while ($j <= 10) {
@@ -128,43 +154,57 @@
     echo "</table>";
     echo " <br>el total de los numeros pares es : ".$total;
     echo " <br>el total de los numeros Inpares es : ".$resultado;
-    echo "<hr>";
     echo "<br><br>";
-    echo "<b>Inprecion de los numeros de el 1 - 10 </b><br><br>";
-    echo "<table border = '2'>";
-        $j=1;
-        while ($j <= 10) {
-            $rand= rand(-100 , 100);
-            echo " <td> ". $rand. "</td>";
-            $j=$j+1;
-            if($rand%2==0)
+echo"<hr>";
+echo "<b> 9)
+            - Total de números positivos pares generados.
+            - Total de números positivos impares generados.
+            - Total de números negativos pares generados.
+            - Total de números negativos impares generados.</b><br><br>";
+                $n= 1;
+      $acumulador3=0;
+      $acumulador4=0;
+      $acumulador5=0;
+      $acumulador6=0;
+      
+      echo "<br>Números: ";
+      echo "<table border = '1' cellpadding='4'><br>";
+      while($n<=10)
+      {
+        $nn=rand(-100,100);
+            echo $nn."&nbsp;&nbsp;";
+            if($nn%2==0)
             {
-                if($rand<0)
-                {
-                    $total+=$rand;
-                }else
-                {
-                    $totaln+=$rand;
-                }
-                
+                if($nn>=0){
+                    $acumulador3++;
+                }else{
+                    $acumulador5++;
+                }		 		
             }else 
             {
-                if($rand<0)
+                if($nn>=0)
                 {
-                    $resultado+=$rand;
+                    $acumulador4++;
                 }else
                 {
-                    $resultadon+=$rand;
-                }
+                    $acumulador6++;
+                }	
             }
-        }
-        
-    echo "</table>";
-    echo " <br>el total de los numeros pares es : ".$total;
-    echo " <br>el total de los numeros pares es : ".$totaln;
-    echo " <br>el total de los numeros Inpares es : ".$resultado;
-    echo " <br>el total de los numeros Inpares es : ".$resultadon;
-
+            $n++;
+      }
+      echo "</table>";
+      echo "<br><b>REPORTE FINAL2</b>";
+      echo "<table border=1>";
+      echo "<tr><td>Total de pares positivos:</td><td>". $acumulador3 ."</td></tr>";
+      echo "<tr><td>Total de impares positivos:</td><td>". $acumulador4 ."</td></tr>";
+      echo "<tr><td>Total de pares negativos:</td><td>". $acumulador5 ."</td></tr>";
+      echo "<tr><td>Total de impares negativos:</td><td>". $acumulador6 ."</td></tr>";
+      echo "</table>";
+    
+echo "</tr></table>";
+echo "<table border = '1'><tr>";  
+echo"<hr>";
 
 
     ?>
+    </center>
